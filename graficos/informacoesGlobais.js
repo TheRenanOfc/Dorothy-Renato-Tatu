7d2343146228fc41b5deccdb4f53f2d0f1a82444
TheRenanOfc <162510870+TheRenanOfc@users.noreply.github.com>
2024-12-02 20:08:51 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,24 +1,27 @@
-const url = 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/trabalho/trabalho-dados-gerais.json';
+const url="https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json";
 
-async function visualizarInformacoesGlobais() {
-    const res = await fetch(url);
-    const dados = await res.json();
-    const pessoasEmpregadas = (dados.total_pessoas_conectadas / 1e9).toFixed(2);
-    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9).toFixed(2);
-    const horas = parseInt(dados.tempo_medio);
-    const minutos = Math.round((dados.tempo_medio - horas) * 60);
-    const porcentagemPessoasEmpregadas = ((pessoasEmpregadas / pessoasNoMundo) * 100).toFixed(2);
+async function visualizarInformacoesGlobais(){
+    const resposta = await fetch (url);
+    let dados = await resposta.json();
 
-    const pessoasSemEmprego = (pessoasNoMundo - pessoasEmpregadas).toFixed(2);
-    const porcentagemPessoasSemEmprego = ((pessoasSemEmprego / pessoasNoMundo) * 100).toFixed(2);
-    
-    const paragrafo = document.createElement('p');
-    paragrafo.classList.add('graficos-container__texto');
-    paragrafo.innerHTML = `Você sabia que o mundo tem ${pessoasNoMundo} bilhões de pessoas, e que aproximadamente ${pessoasEmpregadas} bilhões delas estão empregadas, passando, em média, ${horas} horas e ${minutos} minutos semanalmente em horário de trabalho.
-Isso significa que aproximadamente ${porcentagemPessoasSemEmprego}% não estão formalmente trabalhando.`;
+const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+const horas = parseInt(dados.tempo_medio)
+const minutos = Math.round((dados.tempo_medio - horas) * 60)
 
-    const container = document.getElementById('graficos-container');
+const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2);
+
+
+    console.log(dados);
+
+    const paragrafo = document.createElement("p");
+    paragrafo.classList.add("graficos-container__texto");
+    paragrafo.innerHTML = `Você sabia que o mundo tem existem <span>
+    21</span> espécies de tatus e aproximadamente <span>
+    8.1</span> bilhões de pessoas no mundo que estão em busca de um tatu para chamar de seu <span>
+    24 horas por dia</span> e <span>7 dias por semana</span>.<br>Isso significa que aproximadamente <span>
+    ${porcentagemConectada}%</span> pessoas que estão atrás de um Tatu Galinha tunado.`
+    const container = document.getElementById("graficos-container");
     container.appendChild(paragrafo);
 }
-
 visualizarInformacoesGlobais();
