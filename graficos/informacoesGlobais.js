25c20937b78d3b3d1ebaba4796b9f1b39c2b6f78
TheRenanOfc <162510870+TheRenanOfc@users.noreply.github.com>
2024-12-02 20:06:08 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,24 +1,24 @@
-const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/trabalho/trabalho-dados-gerais.json'
+const url = 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/trabalho/trabalho-dados-gerais.json';
 
-async function vizualizarInformacoesGlobais() {
-    const res = await fetch(url)
-    const dados = await res.json()
-    const pessoasEmpregadas = (dados.total_pessoas_conectadas / 1e9)
-    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
-    const horas = parseInt(dados.tempo_medio)
-    const minutos = Math.round((dados.tempo_medio - horas) * 60)
+async function visualizarInformacoesGlobais() {
+    const res = await fetch(url);
+    const dados = await res.json();
+    const pessoasEmpregadas = (dados.total_pessoas_conectadas / 1e9).toFixed(2);
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9).toFixed(2);
+    const horas = parseInt(dados.tempo_medio);
+    const minutos = Math.round((dados.tempo_medio - horas) * 60);
     const porcentagemPessoasEmpregadas = ((pessoasEmpregadas / pessoasNoMundo) * 100).toFixed(2);
 
-    const pessoasSemEmprego = (pessoasNoMundo - pessoasEmpregadas)
-    const porcentagemPessoasSemEmprego= ((pessoasSemEmprego / pessoasNoMundo) * 100).toFixed(2);
+    const pessoasSemEmprego = (pessoasNoMundo - pessoasEmpregadas).toFixed(2);
+    const porcentagemPessoasSemEmprego = ((pessoasSemEmprego / pessoasNoMundo) * 100).toFixed(2);
     
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-container__texto');
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${pessoasNoMundo} bilhões de pessoas, e que aproximadamente ${pessoasEmpregadas} bilhões delas estão empregadas, passando, em média, ${horas} horas e ${minutos} minutos semanalmente em horário de trabalho.
+Isso significa que aproximadamente ${porcentagemPessoasSemEmprego}% não estão formalmente trabalhando.`;
 
-    const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}$ bilhões</span> de pessoas, e que aproximadamente <span>${pessoasEmpregadas} bilhões</span> delas estão empregadas, passando, em média, <span>${horas} horas</span> e <span>${minutos} minutos</span> semanalmente em horário de trabalho.<br>Isso significa que aproximadamente <span>${porcentagemPessoasSemEmprego}</span> não estão formalmente trabalhando.`
-
-    const container = document.getElementById('graficos-container')
+    const container = document.getElementById('graficos-container');
     container.appendChild(paragrafo);
 }
 
-vizualizarInformacoesGlobais();
+visualizarInformacoesGlobais();
